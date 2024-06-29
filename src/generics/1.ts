3da2c1d368d6f3990bd54c0eaecf15cef8e30e90
Valeriy.Gurnak <vmgurnak@gmail.com>
2024-06-29 15:01:01 +0300

    updated

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -1,10 +1,11 @@
 // Типізуйте асинхронну функцію fetchData, яка приймає URL ресурсу та повертає об'єкт з даними. Використовуйте Generics для типізації повернутих даних.
 
 import axios from 'axios';
-async function fetchData(url: string): Promise<{ [key: string]: any }> {
+
+async function fetchData<T>(url: string): Promise<T> {
   try {
-    const response = await axios.get(url);
-    return response.data;
+    const response: { data: T } = await axios.get(url);
+    return response.data as T;
   } catch (error) {
     throw new Error(`Error fetching from ${url}: ${error}`);
   }
